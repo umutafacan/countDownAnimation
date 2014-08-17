1f6890ba333a8833665aa241f17cf0ac7be0f43e
umutafacan <umut-afacan@hotmail.com>
2014-08-17 12:58:26 +0300

    Initial Commit

---
+++ countDownAnimation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  countDownAnimation
+//
+//  Created by umut on 17/08/14.
+//  Copyright (c) 2014 umut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
